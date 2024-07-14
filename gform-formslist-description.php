97d2dd6d65e4dad3b3295fd89a9300693782b030
ash matadeen <ash.matadeen@gmail.com>
2024-07-14 10:41:20 +0100

    initial commit - plugin scaffold

---
+++ gform-formslist-description.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Gravity Forms Forms List Description
+ * Plugin URI:      https://webblerock.com
+ * Description:     Adds a column displaying the description for each form in the admin forms listing.
+ * Author:          Ash Matadeen
+ * Author URI:      https://webblerock.com
+ * Text Domain:     gform-formslist-description
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Gform_Formslist_Description
+ */
+
+// Your code starts here.
