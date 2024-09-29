6d43b1d6d8033a81afd02aa0b424d2d6f12e248a
ash matadeen <ash.matadeen@gmail.com>
2024-09-29 18:31:07 +0100

    use plugin slug for php filename

---
+++ forms-list-description-for-gravity-forms.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name:      Forms List Description for Gravity Forms
+ * Requires Plugins: gravityforms
+ * Plugin URI:       https://ashmatadeen.com/web-developer
+ * Description:      Adds a column displaying the description for each form in the admin forms listing.
+ * Author:           Ash Matadeen
+ * Text Domain:      forms-list-description-for-gravity-forms
+ * Domain Path:      /languages
+ * License:          GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Version:          0.1.0
+ *
+ * @package Gform_Formslist_Description
+ */
+
+if (! defined('ABSPATH') ) {
+    die();
+}
+
+require plugin_dir_path(__FILE__) . 'classes/class-gform-formslist-description.php';
+
+$gform_formslist_description = ( isset($gform_formslist_description) && is_object($gform_formslist_description) ? $gform_formslist_description : new FLDFGF_GformFormsListDescription() );
